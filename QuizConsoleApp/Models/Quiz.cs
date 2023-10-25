2bf3b749139a01792c8451ab3f916bb4db35da22
Rashad133 <tu82907hw@code.edu.az>
2023-10-25 04:50:01 +0400

    taskcommit

---
+++ QuizConsoleApp/Models/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsoleApp.Models
+{
+    internal class Quiz
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+
+
+        public Quiz(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ QuizConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace QuizConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("[1] Create new quiz");
+            Console.WriteLine("[2] Solve a quiz");
+            Console.WriteLine("[3] Show quizzes");
+            Console.WriteLine("[0] Quit");
+
+            Console.WriteLine("=======================");
+            Console.WriteLine("Hello quiz world");
+            Console.WriteLine("=======================");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Sual adini daxil et");
+                    string name = Console.ReadLine();
+                    Console.WriteLine(name);
+                    Console.WriteLine("Sual daxil et");
+                    List<string> question = new List<string>();
+                    Console.WriteLine(question);
+                    break;
+                case "2":
+                    Console.WriteLine("Nece sual elave etmek isteyirsen?");
+                    List
+                    break;
+                case "3":
+                    Console.WriteLine("Sual daxil et:");
+                    
+                    break;
+                case "4":
+                    Console.WriteLine();
+                    break;
+            }
+
+
+        }
+    }
+}
